45310332757bfde257c141c60d5a6533fda4ec54
Allen Hsieh <allenhsieh2007@gmail.com>
2024-08-07 23:12:14 -0700

    driver control

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,19 @@
 #include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::set_text(1, "TYPE SHI");
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
+void disabled() {} // disregard don't delete
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
+void competition_initialize() {
+	//initialize sensors + autonomous selector here
+}
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -74,21 +42,32 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
+	pros::Controller controller(pros::E_CONTROLLER_MASTER);
+	pros::MotorGroup rightDrive({4, 5, -11}, pros::v5::MotorGears::blue, pros::v5::MotorUnits::rotations); 
+	pros::MotorGroup leftDrive({-1, 2, -3}, pros::v5::MotorGears::blue, pros::v5::MotorUnits::rotations); 
+	pros::Motor intake(-21, pros::v5::MotorGears::green, pros::v5::MotorUnits::rotations);
+	pros::Motor elevator(-20, pros::v5::MotorGears::blue, pros::v5::MotorUnits::rotations);
+	pros::adi::Pneumatics mogomech(1, true);
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		int dir = controller.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
+		int turn = controller.get_analog(ANALOG_RIGHT_Y);  // Gets the turn left/right from right joystick
+		leftDrive.move(dir*.6);
+		rightDrive.move(turn*.6); 
+		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+			intake.move(100);
+			elevator.move(100);
+		} else {
+			intake.move(0);
+			elevator.move(0);
+		}
+		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+			mogomech.extend();
+		} 
+		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+			mogomech.retract();
+		}
+		pros::delay(20);                 
 	}
 }
