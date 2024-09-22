875edb28e9fea1ef50c131b86deece6f65a3b12c
potatooeed <160543647+potatooeed@users.noreply.github.com>
2024-09-22 10:59:31 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -95,11 +95,11 @@ void competition_initialize() {
 }
 
 void autonomous() {
-    // set position to x:0, y:0, heading:0
-    chassis.setPose(0, 0, 0);
-    // turn to face heading 90 with a very long timeout
-    chassis.turnToHeading(90, 100000); // we using this to tune angle first today
-
+    	// set position to x:0, y:0, heading:0
+   	chassis.setPose(0, 0, 0);
+   	// turn to face heading 90 with a very long timeout
+    	chassis.turnToHeading(90, 100000); // we using this to tune angle first today
+	//chassis.moveToPoint (10, 10, 0, 4000) //test lateral/angular together after
 
 }
 
