e7dc4c372368cba55238d83235e1e1a420e0d7ed
Allen Hsieh <allenhsieh2007@gmail.com>
2024-09-29 12:22:45 -0700

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -12,42 +12,41 @@ pros::MotorGroup rightMotors({11, 12, -13}, pros::MotorGearset::blue);
 
 pros::Motor intake(14, pros::MotorGears::green, pros::v5::MotorUnits::rotations);
 pros::Motor chain(-19, pros::MotorGears::blue, pros::MotorUnits::rotations);
-pros::Motor lift(-21, pros::MotorGears::green, pros::v5::MotorEncoderUnits::rotations);
+//pros::Motor lift(-21, pros::MotorGears::green, pros::v5::MotorEncoderUnits::rotations);
 pros::adi::Pneumatics mogomech(1, true);
 pros::adi::Pneumatics release(2, false);
 
 pros::Imu imu(4);
-pros::Rotation verticalEnc(16);
+pros::Rotation verticalEnc(-17);
 pros::Rotation horizontalEnc(15);
 
 // tracking wheels
-lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_2, -5.75);
-lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_2, -2.5);
+lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_2, .8);
+lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_2, .6);
 
 //drivetrain settings
 lemlib::Drivetrain drivetrain(&leftMotors, &rightMotors, 
                               11.22, 
                               lemlib::Omniwheel::NEW_325, 
                               360, 
-                              2
-);
+                              2);
 
 // lateral PID controller
-lemlib::ControllerSettings linearController(10, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              3, // derivative gain (kD)
-                                              0, // anti windup
-                                              0, // small error range, in inches
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in inches
-                                              0, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
+lemlib::ControllerSettings linearController(32.8, // proportional gain (kP)
+                                            10, // integral gain (kI)
+                                            97, // derivative gain (kD)
+                                            0, // anti windup
+                                            5, // small error range, in inches
+                                            1000, // small error range timeout, in milliseconds
+                                            10, // large error range, in inches
+                                            2000, // large error range timeout, in milliseconds
+                                            0 // maximum acceleration (slew)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angularController(0, // proportional gain (kP)
+lemlib::ControllerSettings angularController(5.9, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              0, // derivative gain (kD)
+                                              47.7, // derivative gain (kD)
                                               0, // anti windup
                                               0, // small error range, in degrees
                                               0, // small error range timeout, in milliseconds
@@ -86,15 +85,16 @@ void initialize() {
     
 	intake.set_brake_mode(pros::MotorBrake::coast);
 	chain.set_brake_mode(pros::MotorBrake::brake);
-    lift.set_brake_mode(pros::MotorBrake::brake);
+//  lift.set_brake_mode(pros::MotorBrake::brake);
 
     // set position to x:0, y:0, heading:0
-    //chassis.setPose(0, 0, 0);
+    chassis.setPose(0, 0, 0);
    	// turn to face heading 90 with a very long timeout
-    //chassis.turnToHeading(180, 5000); // we using this to tune angle first today
+    //chassis.turnToHeading(90,5000);
+    //chassis.turnToHeading(0, 5000); 
+    //chassis.turnToHeading(180, 100000); 
 
-    //chassis.moveToPoint(0, 10, 5000);
-    
+    chassis.moveToPose(0, 48, 0, 10000);
 }
 
 void disabled() {} // disregard don't delete
@@ -134,7 +134,7 @@ void opcontrol() {
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
             chain.move(-75);
         }
-        //lift
+        /*lift
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
             lift.move(100);
 
@@ -143,7 +143,7 @@ void opcontrol() {
         } else {
             lift.move(0);
         }
-        bool L2State = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+        bool L2State = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);*/
 		//mogomech pneumatics L1
         bool current_L1_state = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
         if (current_L1_state && !last_L1_state) {
