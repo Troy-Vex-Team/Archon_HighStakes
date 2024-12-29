9580c725becdf1f9e853c519a881b0ca24f3cafb
RHNCG21 <rohanchakgupta@gmail.com>
2024-12-28 19:19:43 -0800

    base gui (should work in theory)

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include "pros/motors.hpp"
 #include "pros/rtos.hpp"
 #include "pros/apix.h"
+#include <cstring>
 #include <string>
 
 // CONTROLLER & SENSORS
@@ -89,33 +90,128 @@ void disabled() {} // disregard don't delete
 
 static lv_obj_t *auton_sel_screen = nullptr;
 static lv_obj_t *match_sel_screen = nullptr;
+static lv_obj_t *confirmation_screen = nullptr;
 static lv_obj_t *current_screen = nullptr;
 
-static const char * auton_sel_map[] = {"Match Auton", "Skill Auton", ""};
+static lv_obj_t *match_btnm = nullptr;
+static lv_obj_t *auton_btnm = nullptr;
+
+static const char * auton_sel = nullptr;
+static const char * team = nullptr;
+static const char * side = nullptr;
+static const char * wp = nullptr;
+
+static void create_confirmation_screen(void);
+
+
+static const char * auton_sel_map[] = {"Match Auton", "Skills Auton", ""};
 static const char * match_sel_map[] = {"Red Side", "Blue Side", "\n",
                                       "Ring Side", "Goal Side", "\n",
                                       "Win Point", ""};
 
 static void event_handler(lv_event_t * e) {
     lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * obj = lv_event_get_target(e);
-    
+    lv_obj_t * obj = (lv_obj_t*) lv_event_get_target(e);
+    if (obj == NULL) {
+        LV_LOG_ERROR("Object is NULL\n");
+        return;
+    }
+
+    if(code == LV_EVENT_CLICKED) {
+        lv_obj_t * child = lv_obj_get_child(obj, 0);
+
+        if (child == NULL) {
+            LV_LOG_ERROR("Button has no child\n");
+            return;
+        }
+
+        LV_LOG_USER("Child class: %p\n", lv_obj_get_class(child));
+
+        // Check if the child is a label
+        if (lv_obj_get_class(child) == &lv_label_class) {
+            const char * label = lv_label_get_text(child);
+
+            if (label == NULL) {
+                LV_LOG_ERROR("Label text is NULL\n");
+                return;
+            }
+
+            LV_LOG_USER("Label text: %s\n", label);
+
+            if(strcmp(label, "Confirm") == 0) {
+                auton_sel = lv_btnmatrix_get_btn_text(auton_btnm, lv_btnmatrix_get_selected_btn(auton_btnm));
+
+                if(lv_btnmatrix_has_btn_ctrl(match_btnm, 0, LV_BTNMATRIX_CTRL_CHECKED)) {
+                  team = "Red";
+                } else if (lv_btnmatrix_has_btn_ctrl(match_btnm, 1, LV_BTNMATRIX_CTRL_CHECKED)) {
+                  team = "Blue";
+                }
+
+                if(lv_btnmatrix_has_btn_ctrl(match_btnm, 2, LV_BTNMATRIX_CTRL_CHECKED)) {
+                  side = "Ring Side";
+                } else if (lv_btnmatrix_has_btn_ctrl(match_btnm, 3, LV_BTNMATRIX_CTRL_CHECKED)) {
+                  side = "Goal Side";
+                }
+
+                if(lv_btnmatrix_has_btn_ctrl(match_btnm, 4, LV_BTNMATRIX_CTRL_CHECKED)) {
+                  wp = "Yes";
+                } else {
+                  wp = "No";
+                }
+                
+                if (team == nullptr || side == nullptr || wp == nullptr) {
+                    return;
+                }
+                
+                create_confirmation_screen();
+                lv_scr_load_anim(confirmation_screen, LV_SCR_LOAD_ANIM_FADE_ON, 300, 0, false);
+                current_screen = confirmation_screen;
+            }
+        }
+    }
     if(code == LV_EVENT_VALUE_CHANGED) {
         uint32_t id = lv_btnmatrix_get_selected_btn(obj);
         const char * txt = lv_btnmatrix_get_btn_text(obj, id);
         LV_LOG_USER("%s was pressed\n", txt);
-        
+
+        if(strcmp(txt, "Red Side") == 0) {
+          lv_btnmatrix_clear_btn_ctrl(obj, 1, LV_BTNMATRIX_CTRL_CHECKED);
+        }
+
+        if(strcmp(txt, "Blue Side") == 0) {
+          lv_btnmatrix_clear_btn_ctrl(obj, 0, LV_BTNMATRIX_CTRL_CHECKED);
+        }
+
+        if(strcmp(txt, "Ring Side") == 0) {
+          lv_btnmatrix_clear_btn_ctrl(obj, 3, LV_BTNMATRIX_CTRL_CHECKED);
+        }
+
+        if(strcmp(txt, "Goal Side") == 0) {
+          lv_btnmatrix_clear_btn_ctrl(obj, 2, LV_BTNMATRIX_CTRL_CHECKED);
+        }
+
         if(strcmp(txt, "Match Auton") == 0) {
             lv_scr_load_anim(match_sel_screen, LV_SCR_LOAD_ANIM_FADE_ON, 300, 0, false);
             current_screen = match_sel_screen;
+        }
+
+        if(strcmp(txt, "Skills Auton") == 0) {
+            auton_sel = "Skills Auton";
+            team = "N/A";
+            side = "N/A";
+            wp = "N/A";
+            create_confirmation_screen();
+
+            lv_scr_load_anim(confirmation_screen, LV_SCR_LOAD_ANIM_FADE_ON, 300, 0, false);
+            current_screen = confirmation_screen;
         }
     }
 }
 
 static void create_auton_sel_screen() {
     auton_sel_screen = lv_obj_create(NULL);
-    
-    lv_obj_t * auton_btnm = lv_btnmatrix_create(auton_sel_screen);
+
+    auton_btnm = lv_btnmatrix_create(auton_sel_screen);
     lv_obj_set_size(auton_btnm, 400, 192);
     lv_btnmatrix_set_map(auton_btnm, auton_sel_map);
     lv_obj_align(auton_btnm, LV_ALIGN_CENTER, 0, 0);
@@ -125,10 +221,10 @@ static void create_auton_sel_screen() {
 static void create_match_sel_screen() {
     match_sel_screen = lv_obj_create(NULL);
 
-    lv_obj_t * match_btnm = lv_btnmatrix_create(match_sel_screen);
+    match_btnm = lv_btnmatrix_create(match_sel_screen);
     lv_obj_set_size(match_btnm, 400, 170);
     lv_btnmatrix_set_map(match_btnm, match_sel_map);
-    
+
     // Make first 5 buttons checkable
     for (int i = 0; i < 5; i++) {
         lv_btnmatrix_set_btn_ctrl(match_btnm, i, LV_BTNMATRIX_CTRL_CHECKABLE);
@@ -150,6 +246,17 @@ void lvgl_display_task_fn(void* param) {
         lv_task_handler();
         pros::delay(10);
     }
+}
+
+static void create_confirmation_screen() {
+  confirmation_screen = lv_obj_create(NULL);
+
+  char text_buffer[200];
+  snprintf(text_buffer, sizeof(text_buffer), "%s\nTeam: %s\nSide: %s\nWP: %s\n", auton_sel, team, side, wp);
+
+  lv_obj_t * confirm_label = lv_label_create(confirmation_screen);
+  lv_label_set_text(confirm_label, text_buffer);
+  lv_obj_align(confirm_label, LV_ALIGN_CENTER, 0, 0);
 }
 
 void initialize_display() {
@@ -194,6 +301,68 @@ void initialize() {
 }
 
 void autonomous() {
+    if (strcmp(auton_sel, "Match Auton") == 0) {
+
+        if (team == nullptr || side == nullptr || wp == nullptr) {
+            return;
+        }
+
+        if (strcmp(team, "Red") == 0) {
+            if (strcmp(side, "Goal Side") == 0) {
+                if (strcmp(wp, "Yes") == 0) {
+
+                    // RED GOAL SIDE WITH WIN POINT
+
+                } else {
+
+                    // RED GOAL SIDE NO WIN POINT
+
+                }
+            } else {
+                if (strcmp(wp, "Yes") == 0) {
+                    
+                    // RED RING SIDE WITH WIN POINT
+
+                } else {
+
+                    // RED RING SIDE NO WIN POINT
+
+                }
+            }
+        } else {
+            if (strcmp(side, "Goal Side") == 0) {
+                if (strcmp(wp, "Yes") == 0) {
+
+                    // BLUE GOAL SIDE WITH WIN POINT
+
+                } else {
+
+                    // BLUE GOAL SIDE NO WIN POINT
+
+                }
+            } else {
+                if (strcmp(wp, "Yes") == 0) {
+                    
+                    // BLUE RING SIDE WITH WIN POINT
+
+                } else {
+
+                    // BLUE RING SIDE NO WIN POINT
+
+                }
+            }
+        }
+    } else {
+
+        // SKILLS AUTON
+
+    }
+
+
+
+
+
+
     //red ring
     chassis.setPose(0, 0 , 0);
     mogomech.extend();
